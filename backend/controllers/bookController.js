c42f5356f772d31d2eace55115f4d27f4a50c90a
mehuldotdev <mehehehul748@gmail.com>
2025-09-04 11:53:29 +0530

    admin controls

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv"
 dotenv.config();
 import Book from "../models/books.js";
-import User from "../models/user.js";
+// Creating the book
 
 export const addBook = async (req,res) => {
     try {
@@ -29,5 +29,113 @@ export const addBook = async (req,res) => {
     } catch (error) {
         console.log(error);
         res.status(500).json({message: error.message})
+    }
+}
+
+// Updating the book
+
+export const updateBook = async (req, res) => {
+  try {
+    const { id } = req.user;
+    const user = await User.findById(id);
+
+    if (!user || user.role !== "admin") {
+      return res.status(403).json({ message: "Admin operations can't be performed" });
+    }
+
+    const { bookId } = req.params; // get book id from URL
+
+    const updatedBook = await Book.findByIdAndUpdate(
+      bookId,
+      {
+        url: req.body.url,
+        desc: req.body.desc,
+        title: req.body.title,
+        author: req.body.author,
+        price: req.body.price,
+        language: req.body.language,
+      },
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedBook) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    res.status(200).json({
+      message: "Book updated successfully",
+      book: updatedBook,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Deleting the book
+
+export const deleteBook = async (req,res) => {
+    try {
+        const {id} = req.user;
+        const user = await User.findById(id);
+    
+        if(user.role!=="admin" || !user){
+            return res.status(403).json({ message: "Admin operations can't be performed" });
+        }
+    
+        const {bookId} = req.params;
+    
+        const deletedBook = await Book.findByIdAndDelete(bookId)
+    
+        if(!deletedBook){
+            return res.status(400).json({message: "Book not found"})
+        }
+    
+        return res.status(200).json({message: "Book deleted successfully"})
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({message: error.message})
+    }
+
+}
+
+// Fetch all books
+
+export const getBooks = async (req,res) => {
+    try {
+
+        const books = await Book.find().sort({createdAt: -1}).limit(4);
+        res.status(200).json({
+            message: "All books shown",
+            books: books
+        })
+
+    } catch (error) {
+
+      console.log(error);
+      res.status(500).json({message: error.message})   
+
+    }
+}
+
+// Fetch book as per id
+
+export const getBookById = async(req,res) => {
+    try {
+        
+        const {bookId} = req.params
+
+        const book = await Book.findById(bookId);
+
+        if(!book){
+            return res.json(400).json({message: "Book not found!"})
+        }
+
+        res.status(200).json(book)
+
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({message: error.message}) 
     }
 }
--- backend/routes/book.js
+++ backend/routes/book.js
@@ -1,10 +1,18 @@
 import express from "express"
 import { protect } from "../middleware/authMiddleware.js"
-import { addBook } from "../controllers/bookController.js"
+import { addBook, deleteBook, getBookById, getBooks, updateBook } from "../controllers/bookController.js"
 
 
 const router = express.Router()
 
 router.post('/add-book', protect, addBook)
+
+router.put('/update-book/:bookId', protect, updateBook)
+
+router.delete('/delete-book:bookId', protect, deleteBook)
+
+router.get('/', getBooks)
+
+router.get('/:bookId', getBookById)
 
 export default router
